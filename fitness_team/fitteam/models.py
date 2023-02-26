2aafa00dff5dca9ec5dc1214fdc4fc085750374f
adamperformance <104623807+adamperformance@users.noreply.github.com>
2023-02-26 19:34:47 +0100

    Setup Django app and login/register/logout funct.

---
+++ fitness_team/fitteam/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from tkinter import CASCADE
+from django.contrib.auth.models import AbstractUser
+
+
+# Create your models here.
+
+
+class User(AbstractUser):
+    pass
+
+# class BasicInfo(models.Model):
+#     pass
+
